c293cd675e78cf23b9b4b6b8ed4f295ee45703de
Vinicius Silva <vinicius.silva.1046664@sga.pucminas.br>
2024-01-05 14:45:57 -0300

    Basic tests structure implemented

--- vinigit/git/tests.py
+++ vinigit/git/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from git.models import *
 
-# Create your tests here.
+class RepositoryTestCase(TestCase):
+    def setUp(self):
+        
+        #Repository.objects.create()
+        #Repository.objects.create()
+        pass
+    
+    def test_create_repository():
+        pass
+    
+class PullRequestTestCase(TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def test_create_pullrequest():
+        pass
+    
+class ReleaseTestCase(TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def test_create_release():
+        pass
